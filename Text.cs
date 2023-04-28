1d9591d481cb67414e6a968cc38fbe54a3c45842
saubgayavishwanthi <131455225+saubgayavishwanthi@users.noreply.github.com>
2023-04-28 09:25:21 +0530

    i'm adding to display star patten using while loop

---
+++ Text.cs
@@ -0,0 +1,18 @@
+Console.WriteLine("hello world");
+public static void Main(string[] args)
+{
+  Console.WriteLine("Hello");
+  int i =0,j=0;
+  while (i<2)
+  {
+      j=0
+      while(j<5)
+     {
+      Console.Write("*");
+      j++;
+    }
+    i++;
+    }
+    
+    }
+    Console.ReadLine();
